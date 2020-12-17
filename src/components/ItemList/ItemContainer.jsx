46644807a1d1246dcaa5f251a8fd30d4e3b36966
Walter <walter_santillan@hotmail.com>
2020-12-17 18:11:51 -0300

    tarjeta producto con botones + y - y contador

---
+++ src/components/ItemList/ItemContainer.jsx
@@ -0,0 +1,31 @@
+import { Card } from "react-bootstrap";
+import { useState } from "react";
+
+function ItemContainer() {
+  const [cant, setCant] = useState(1);
+
+  const restarCantidad = () => {
+    if (cant > 1) {
+      setCant(cant - 1);
+    }
+  };
+
+  return (
+    <Card style={{ width: "10rem" }}>
+      <Card.Img variant="top" src="https://via.placeholder.com/120" alt="" />
+      <Card.Body>
+        <Card.Title>Producto</Card.Title>
+        <button className="btn" variant="light" onClick={restarCantidad}>
+          -
+        </button>
+        <span>{cant}</span>
+        <button className="btn" variant="light" onClick={() => setCant(cant + 1)}>
+          +
+        </button>
+        {/* <input type="text" value={cant} /> */}
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ItemContainer;
--- src/components/ItemList/ItemListContainer.jsx
+++ src/components/ItemList/ItemListContainer.jsx
@@ -1,6 +1,8 @@
+import ItemContainer from "./ItemContainer";
+
 function ItemListContainer () {
     return (
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium ex laboriosam numquam deleniti enim provident, similique, atque minus eligendi, vel sed aspernatur eaque. Sed molestias voluptas temporibus ducimus explicabo? Dolores.</p>
+        <ItemContainer />
     )
 }
 
