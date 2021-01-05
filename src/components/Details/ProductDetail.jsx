4b7cf4ffc7bcd2767889cb0de23e206b38d2fdc6
Walter <walter_santillan@hotmail.com>
2021-01-05 18:00:03 -0300

    Ejercicio 9  - Eventos

--- src/components/Details/ProductDetail.jsx
+++ src/components/Details/ProductDetail.jsx
@@ -1,15 +1,43 @@
-const ProductDetail = ({item}) => {
-    return (
-      <article>
-        <h1>
-          {item.nombre} - {item.id}
-        </h1>
-        <img src={item.foto} alt="" />
-        <p>{item.descripcion}</p>
-        <p>{item.precio}</p>
-        <button>Agregar al carrito</button>
-      </article>
-    );
-}
+import { useState } from "react";
+import "./ProductDetail.css";
 
-export default ProductDetail
+const ProductDetail = ({ item }) => {
+  const [cant, setCant] = useState(1);
+
+  const onAdd = () => {
+    alert(`Agregaste $(cant) productos al carrito`);
+  };
+
+  const handleClickRestar = () => {
+    if (cant > 1) {
+      setCant(cant - 1);
+    }
+  };
+
+  return (
+    <article className="producto">
+      <h1>
+        {item.nombre} - {item.id}
+      </h1>
+      <img src={item.foto} alt="" />
+      <p>{item.descripcion}</p>
+      <p>{item.precio}</p>
+      <div className="cant">
+        <button
+          disabled={cant === 1 ? "disabled" : null}
+          onClick={handleClickRestar}
+        >
+          -
+        </button>
+        <input type="text" value={cant} readOnly />
+        <button onClick={() => setCant(cant + 1)}>+</button>
+      </div>
+
+      <button className="btn" onClick={onAdd}>
+        Agregar al carrito
+      </button>
+    </article>
+  );
+};
+
+export default ProductDetail;
